9444d3c5938d6ed96e7f6947ebdf320d4f9f8069
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-13 04:37:38 -0700

    puts 2d1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/** 
-*main -Entry
-*Return: Always 0 (Success)
-*/
-int main(void)
+/**
+ * main - Entry pont
+ * Return: Always 0 (success) 
+ */
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingua puzzle");
+	return (0);
 }
+
