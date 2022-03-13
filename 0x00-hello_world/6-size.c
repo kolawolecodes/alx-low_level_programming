0090efe90c99681a0d36091c7bbae28a9654a442
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-13 06:03:14 -0700

    size d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu bytes\n", (unsigned long)sizeof(i2));
-	printf("Size of a long long int: %lu bytes\n", (unsigned long)sizeof(i3));
-	printf("Size of a float: %lu bytes\n", (unsigned long)sizeof (f));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i2));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(i3));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof (f));
 	return (0);
 }
