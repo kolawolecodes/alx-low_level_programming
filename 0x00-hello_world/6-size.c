db7ea4a03e54585a89b5153f5fa28ba305511344
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-13 06:07:21 -0700

    size d1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i2));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(i3));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof (f));
-	return (0);
+	return 0;
 }
