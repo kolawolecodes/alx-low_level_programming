cc93534227bd4dc0570edd6fa4f3badfbf977d3a
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-13 07:56:43 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include <unistdio.h>
+/**
+ * main - Entry point
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+	return (0);
+}
