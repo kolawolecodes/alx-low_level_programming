c246fedd9e5c508f85a0234c3eb90c6179b693d1
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-14 11:48:00 -0700

    last digit command

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description:Get the a random value of n where
+ * the last digit is printed if it is greater than five
+ * equals zero and less than six but not zero
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int n; int a;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	a = n % 10
+	if (a > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	else if (a == 0)
+		printf("Last digit of %d is %d and is 0\n", n, a);
+	else (a < 6 && a != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+	return (0);
+}
