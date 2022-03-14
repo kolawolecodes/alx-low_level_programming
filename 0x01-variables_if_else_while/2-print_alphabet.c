54e876b4b5b24952bba6d4f78295c1e6bb9dcf25
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-14 12:53:47 -0700

    print lower case letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description:print alphabet a to z in lower case
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
