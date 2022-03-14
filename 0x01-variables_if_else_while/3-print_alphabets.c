53482e7bd1da61057c02bd9cca3a2df3f73b0eaf
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-14 14:05:56 -0700

    print lower and upper case alphabet 2d

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,11 +14,11 @@ int main(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
+		putchar("\n");
 	}
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
 	}
-	putchar("\n");
 	return (0);
 }
