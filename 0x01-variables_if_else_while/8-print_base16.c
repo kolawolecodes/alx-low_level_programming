37bdd904fe513e85e6dec7dfd667d83bcd959a34
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-14 20:35:10 -0700

    lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	char base_16;
+
+	for (base_16 = 48; base_16 <= 57; base_16++)
+	{
+		putchar(base_16);
+	}
+	putchar('\n');
+	return (0);
+}
