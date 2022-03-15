44da77ab3725c7dca49c6e6b70bd75744dedbf69
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-14 21:26:16 -0700

    combo 2c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,16 +11,13 @@ int main(void)
 	for (number = 48; number <=57; number++)
 	{
 		putchar(number);
-	}
 	if (number == 57)
 	{
 	break;
 	}
-	{
-		putchar(' , ');
-		putchar (' ');
+	putchar(', ');
+	putchar (' ');
 	}
-
 	putchar('\n');
 	return (0);
 }
