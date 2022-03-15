6ceec6fd42c23d35d3bd8cea41714db41fdcc14e
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-14 21:45:00 -0700

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	if (alphabet == 'q')
-	continue;
-	else if (alphabet == 'e')
-	continue;
 	{
+		if (alphabet == 'q')
+		continue;
+		else if (alphabet == 'e')
+		continue;
 		putchar(alphabet);
 	}
 	putchar('\n');
