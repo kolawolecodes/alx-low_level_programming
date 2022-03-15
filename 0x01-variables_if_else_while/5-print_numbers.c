9cf823b17930f8806c47d8376e26419f6c7f9969
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-14 19:31:32 -0700

    numbers e

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 /**
 * main - main block
 * Description:print numbers to base 10
-* Return 0 (Success)
+* Return:Always 0 (Success)
 */
 int main(void)
 {
@@ -10,7 +10,7 @@ int main(void)
 	
 	for (i = 0; i < 10; i++)
 	{
-	printf("%i", i);
+		printf("%i", i);
 	}
 	printf("\n");
 	return (0);
