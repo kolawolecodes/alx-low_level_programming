65ecee8c21a2b66385e1815676b8b144ca25376e
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-14 17:21:53 -0700

    print lowercase with q and e update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch = 'z' ch++)
-	if (ch == q)
+	for (ch = 'a'; ch = 'z'; ch++)
+	if (ch == 'q')
 		continue;
-	else if (ch == e)
+	else if (ch == 'e')
 		continue;
 	{
-		putchar(ch)
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
