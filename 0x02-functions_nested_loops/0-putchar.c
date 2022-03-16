2e8e05bae45d9a14429b378587c020bb97b8d431
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-16 14:35:44 +0000

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Result: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C-function nexted loop Readme
