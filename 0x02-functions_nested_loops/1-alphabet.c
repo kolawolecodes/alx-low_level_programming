e30074c1db09b5c8256adb87bf37c6b4a17e4396
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-16 16:44:57 +0000

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
 #include "main.h"
+
 /**
- * main - main block
  * print_alphabet - print alphabet lowercase
  *
  * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
-	char ch;
+char alphabet;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
 }
