cacc8a3f7284691df1038adfa272a5f9decb0687
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-17 04:29:28 +0000

    largest number a

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+#include "main.h"
 #include <stdio.h>
 /**
  * largest_number - returns the largest of 3 numbers
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a >= b && a >= c)
+if (a > b && b >= c)
 {
 largest = a;
 }
-else if (b >= a && b >= c)
+else if (b > a && a >= c)
 {
 largest = b;
 }
