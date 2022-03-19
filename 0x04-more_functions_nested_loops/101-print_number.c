73308989d8daf617e85072c0e6773da4ff19b56f
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-19 02:07:59 +0000

    function that print integers update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
 * print_number - that prints an integer
-*@n: print int
-* Return: Always 0.
+* @n: print int
+* Return: Always 0 (success)
 */
 void print_number(int n)
 {
 unsigned int j;
+
 if (n < 0)
 {
 j = -n;
@@ -16,9 +17,9 @@ else
 {
 j = n;
 }
-if (j/10)
+if (j / 10)
 {
-print_number(j/10);
+print_number(j / 10);
 }
 _putchar((j % 10) + '0');
 }
