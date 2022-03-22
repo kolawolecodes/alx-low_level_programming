4527b4ad46b2bd91959b7cab2896f3dc569030ea
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-22 09:06:07 -0700

    print reverse of string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,21 +5,21 @@
  *
  *Return: void which means our answer is correct
  */
+
 void print_rev(char *s)
 {
 int i;
 
 i = 0;
-
 while (s[i] != '\0')
 {
 i++;
 }
-for (i = i + 1; i >= 0; i--)
+
+for (i = i - 1 ; i >= 0; i--)
 {
 _putchar (s[i]);
 }
-{
+
 _putchar ('\n');
-}
 }
