43e686080e62414d9b4a528fe70ceb83416c30ba
kolawolecodes <pops.iconicgroup@gmail.com>
2022-03-22 06:37:57 -0700

    reset int address

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - main block
- * Take pointer of an int and update value as 98
+ * reset_to_98 - update an intiger value to 98
+ * @n: Apointer to an int int that will be updated
  * Return: Always 0 (success)
  */
 void reset_to_98(int *n)
 {
-*n = 98;
+*n = 98
 }
