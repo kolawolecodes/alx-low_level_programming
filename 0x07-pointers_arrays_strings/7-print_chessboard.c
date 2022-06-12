2ebc3898c34469d23284c104a6c73b26ac9565bb
kolawolecodes <pops.iconicgroup@gmail.com>
2022-06-12 10:19:13 +0000

    chess board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,39 +1,39 @@
 #include "main.h"
-
 /**
- * print_chessboard - print the chessboard, 2d array
- * @a: 2d array of chars
- *Return: void
+ * print_chessboard - prints the values in the array
+ * @a: character to be inputted
  */
+
 void print_chessboard(char (*a)[8])
 {
-	/*Declaring variables*/
 	int i, j;
-	char lower_s[] = {114, 107, 98, 113, 107, 98, 107, 114};
-	char upper_s[] = {82, 75, 66, 81, 75, 66, 75, 82};
+	char a[] = {114, 107, 98, 113, 107, 98, 107, 114};
+	/*char upper_s[] = {82, 75, 66, 81, 75, 66, 75, 82};*/
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if (i == 1)
+			if (i < 1)
 			{
-				_putchar(80);
+				_putchar(a[j]);
 			}
-			else if (i < 1)
-			{
-				_putchar(upper_s[j]);
-			}
-			else if (i == 6)
+			else if (i == 1)
 			{
 				_putchar(112);
 			}
+			else if (i == 6)
+			{
+				_putchar(80);
+			}
 			else if (i == 7)
 			{
-				_putchar(lower_s[j]);
+				_putchar(a[j] - 32);
 			}
 			else
-				_putchar(' ');	
+			{
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
