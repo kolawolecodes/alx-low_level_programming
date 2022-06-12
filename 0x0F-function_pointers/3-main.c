73393c30c4b90c907ebfafae7991450e683491d1
kolawolecodes <pops.iconicgroup@gmail.com>
2022-06-12 11:04:05 +0000

    calculator main function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,46 +1,36 @@
 #include "3-calc.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * main - program that perfroms simple operations
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * main - entry point
+ * @argc: argument
+ * @argv: element in argument
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
-
+	int num1, num2;
+	char *st;
+	/*key in requirements*/
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
-	func = get_op_func(argv[2]);
-
-	if (!func)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
-
+	/*initialize num1 and num2 with argv[1 and 3]*/
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	st = (argv[2]);/*set address st to arv[2]*/
+	if (!get_op_func(st))/*if argument st is NULL*/
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", get_op_func(st)(num1, num2));/*print result of func.*/
 	return (0);
 }
