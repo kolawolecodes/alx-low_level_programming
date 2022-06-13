7e8a2ede7d746804ebce14678f25d08c6d923606
kolawolecodes <pops.iconicgroup@gmail.com>
2022-06-13 20:46:41 +0000

    make me win modified

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
-sudo LD_PRELOAD=$LD_LIBRARY_PATH ./gm
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
